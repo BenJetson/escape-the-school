6d463281e48ce637e1d1624d3c5258e97e181e37
Ben Godfrey <bgodfr5236@greenvilleschools.us>
2016-04-26 11:12:36 -0400

    Added time-sensetive intersects for teachers and time-sensetive speed penalties.

--- platformer.py
+++ platformer.py
@@ -2,6 +2,8 @@
 
 import pygame
 import random
+import calendar
+import time
 import intersects
 from graphic_handler import *
 
@@ -49,6 +51,10 @@ GRAVITY = 0.4
 TERMINAL_VELOCITY = 10
 
 
+def get_current_time():
+    return calendar.timegm(time.gmtime())
+
+
 class Student:
 
     def __init__(self, x, y, img):
@@ -61,6 +67,7 @@ class Student:
         self.vx = 0
         self.vy = 0
         self.speed = H_SPEED
+        self.temp_speed_changes = []
 
     def get_rect(self):
         return [self.x, self.y, self.w, self.h]
@@ -93,9 +100,15 @@ class Student:
         self.vx = 0
 
     def apply_gravity(self):
-         self.vy += GRAVITY
-         self.vy = min(self.vy, TERMINAL_VELOCITY)
-         
+        self.vy += GRAVITY
+        self.vy = min(self.vy, TERMINAL_VELOCITY)
+
+    def change_speed_temp(self, expiry_time, amount):
+        self.temp_speed_changes.append({
+            "expiryTime" : expiry_time,
+            "changeAmount" : amount
+        });
+
     def process_platforms(self, platforms):
         self.x += self.vx
 
@@ -156,23 +169,33 @@ class Student:
     def process_teachers(self, teachers):
         student_rect = self.get_rect()
 
-        is_touching = False
+        # is_touching = False
             
         for t in teachers:
             teachers_rect = t.get_rect()
 
-            if intersects.rect_rect(student_rect, teachers_rect):
+            if t.is_touching(student_rect):
                     print("bonk!")
-                    is_touching = True
+                    # is_touching = True
 
-        if is_touching:
-            self.speed = H_SPEED - 1
-        else:
-            self.speed = H_SPEED
+                    self.change_speed_temp(get_current_time() + 5, H_SPEED/2)
             
         print(self.speed)
-        
+
+    def process_speed_changes(self):
+        self.speed = H_SPEED
+        current_time = get_current_time()
+
+        for p in self.temp_speed_changes:
+            if not (p['expiryTime'] < current_time):
+                self.speed -= p['changeAmount']
+            else:
+                self.temp_speed_changes.remove(p)
+
+        self.speed = 1 if self.speed < 1 else self.speed
+
     def update(self, platforms, teachers):
+        self.process_speed_changes()
         self.apply_gravity()
         self.process_platforms(platforms)
         self.check_screen_edges()
@@ -193,9 +216,22 @@ class OtherPeople:
         self.w = self.img.get_width()
         self.h = self.img.get_height()
         self.platform_bound = platform_bound
+        self.is_untouchable = False
+        self.last_touch = 0
 
         self.vx = vx
         self.vy = 0
+
+    def is_touching(self, other_rect, dont_set_flag=False, ignore_untouchable=False):
+        if (not self.is_untouchable) or ignore_untouchable:
+            if intersects.rect_rect(self.get_rect(), other_rect):
+                if not dont_set_flag:
+                    self.is_untouchable = True
+                    self.last_touch = get_current_time()
+
+                return True
+
+        return False
 
     def move_and_process_platforms(self, platforms):
         self.x += self.vx
@@ -238,8 +274,12 @@ class OtherPeople:
                     self.y = p.y + p.h
                 self.vy = 0
 
+    def process_touchability(self):
+        if self.last_touch + 5 == get_current_time():
+            self.is_untouchable = False
 
     def update(self, platforms):
+        self.process_touchability()
         self.move_and_process_platforms(platforms)
 
     def get_rect(self):
